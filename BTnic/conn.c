3ca55dc6b38d382284f500f28e3cb08486199314
mattreba <mattreba@786cebc4-e6f9-11dd-9c15-fdd50ba39c1e>
2010-10-22 00:54:09 +0000

    Linux-based BTnic, Work in progress, v0.1

--- BTnic/conn.c
+++ BTnic/conn.c
@@ -1,6 +1,13 @@
-#define BUILD 354 
-/*
-   Copyright (C) 2009, 2010 Matt Reba, Jermeiah Dillingham
+/*  
+  BTnic - BrewTroller Web Service Gateway
+
+  conn.c - connection handling functions
+
+  Copyright (C)2010 Open Source Control Systems, Inc.
+
+  Based on source from:
+  * OpenMODBUS/TCP to RS-232/485 MODBUS RTU gateway
+  * Copyright (c) 2002-2003, Victor Antonovich (avmlink@vlink.ru)
 
     This file is part of BrewTroller.
 
@@ -17,43 +24,10 @@
     You should have received a copy of the GNU General Public License
     along with BrewTroller.  If not, see <http://www.gnu.org/licenses/>.
 
-BrewLogger - SD Card Logger for BrewTroller
+
+BrewTroller - Open Source Brewing Computer
 Software Lead: Matt Reba (matt_AT_brewtroller_DOT_com)
 Hardware Lead: Jeremiah Dillingham (jeremiah_AT_brewtroller_DOT_com)
 
 Documentation, Forums and more information available at http://www.brewtroller.com
-
-Compiled on Arduino-0017 (http://arduino.cc/en/Main/Software)
-With Sanguino Software v1.4 (http://code.google.com/p/sanguino/downloads/list)
-using PID Library v0.6 (Beta 6) (http://www.arduino.cc/playground/Code/PIDLibrary)
-using OneWire Library (http://www.arduino.cc/playground/Learning/OneWire)
 */
-
-#include "WProgram.h"
-#include <avr/pgmspace.h>
-#include <microfat2.h>
-#include <mmc.h>
-#include <DevicePrint.h>
-
-#define PIN_LEDRED 3
-#define PIN_LEDGRN 4
-#define PIN_SWITCH 2
-#define SWITCH_INT 0
-
-char buf[11];
-boolean enterStatus;
-DevicePrint dp;
-
-void setup() {
-  Serial.begin(9600);
-  pinMode(PIN_LEDRED, OUTPUT);
-  pinMode(PIN_LEDGRN, OUTPUT);
-  pinMode(PIN_SWITCH, INPUT);
-  attachInterrupt(SWITCH_INT, doEnter, CHANGE);
-  //initSD();
-}
-
-void loop() {
-  chkMsg();
-
-}
