717d2c3885e198ac62715ebffa5704c95589819f
mattreba <mattreba@786cebc4-e6f9-11dd-9c15-fdd50ba39c1e>
2010-10-22 00:54:09 +0000

    Linux-based BTnic, Work in progress, v0.1

---
+++ trunk/BTnic/log.c
@@ -0,0 +1,33 @@
+/*  
+  BTnic - BrewTroller Web Service Gateway
+
+  log.c - logging functions
+
+  Copyright (C)2010 Open Source Control Systems, Inc.
+
+  Based on source from:
+  * OpenMODBUS/TCP to RS-232/485 MODBUS RTU gateway
+  * Copyright (c) 2002-2003, Victor Antonovich (avmlink@vlink.ru)
+
+    This file is part of BrewTroller.
+
+    BrewTroller is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    BrewTroller is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with BrewTroller.  If not, see <http://www.gnu.org/licenses/>.
+
+
+BrewTroller - Open Source Brewing Computer
+Software Lead: Matt Reba (matt_AT_brewtroller_DOT_com)
+Hardware Lead: Jeremiah Dillingham (jeremiah_AT_brewtroller_DOT_com)
+
+Documentation, Forums and more information available at http://www.brewtroller.com
+*/
---
+++ trunk/BTnic/sig.h
@@ -0,0 +1,46 @@
+/*  
+  BTnic - BrewTroller Web Service Gateway
+
+  sig.h - signal handling functions
+
+  Copyright (C)2010 Open Source Control Systems, Inc.
+
+  Based on source from:
+  * OpenMODBUS/TCP to RS-232/485 MODBUS RTU gateway
+  * Copyright (c) 2002-2003, Victor Antonovich (avmlink@vlink.ru)
+
+    This file is part of BrewTroller.
+
+    BrewTroller is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    BrewTroller is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with BrewTroller.  If not, see <http://www.gnu.org/licenses/>.
+
+
+BrewTroller - Open Source Brewing Computer
+Software Lead: Matt Reba (matt_AT_brewtroller_DOT_com)
+Hardware Lead: Jeremiah Dillingham (jeremiah_AT_brewtroller_DOT_com)
+
+Documentation, Forums and more information available at http://www.brewtroller.com
+*/
+
+#ifndef _SIG_H
+#define _SIG_H
+
+#include "globals.h"
+#include "log.h"
+
+/* prototypes */
+extern volatile sig_atomic_t sig_flag;
+void sig_init(void);
+void sig_exec(void);
+
+#endif /* _SIG_H */
