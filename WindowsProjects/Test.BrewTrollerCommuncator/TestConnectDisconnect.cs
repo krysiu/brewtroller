8f969707c3265896916e1447dd9a2e4094b4ea37
tomontee <tomontee@gmail.com>
2010-10-23 14:34:48 +0000

    NUnit assembly for BT communication tests. These tests communicate with a active BT and will use the BT's configured com mode. This first check-in includes that basic Get/Set configuration functions and the Get/Set Prog functions.

---
+++ WindowsProjects/Test.BrewTrollerCommuncator/TestConnectDisconnect.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using BrewTrollerCommunicator;
+
+
+namespace Test.BrewTrollerCommuncator
+{
+	[TestFixture]
+	[Ignore]
+	public class TestConnectDisconnect
+	{
+
+		BTCommunicator _btCom;
+
+		public TestConnectDisconnect()
+		{
+			_btCom = new BTCommunicator(); 
+		}
+
+		// run before all test have run
+		[TestFixtureSetUp]
+		public void Setup()
+		{
+
+		}
+
+		// run after all test have run
+		[TestFixtureTearDown]
+		public void FixtureTearDown()
+		{
+
+		}
+
+		// run before each test
+		[SetUp]
+		public void SetUp()
+		{
+
+		}
+
+		// run after each test
+		[TearDown]
+		public void TearDown()
+		{
+		}
+
+		[Test]
+		public void Connect()
+		{
+			_btCom.Connect("COM10,115200,N,8,1");
+			Assert.IsTrue(_btCom.IsConnected);
+			Assert.AreEqual(_btCom.Version.ComType, BTComType.BTNic);
+			Assert.AreEqual(_btCom.Version.ComSchema, 20);
+			Assert.AreEqual(_btCom.Version.BuildNumber, 574, "Build number is not correct.");
+			Assert.AreEqual(_btCom.Version.Version,"2.1");
+		}
+
+		[Test]
+		public void Disconnect()
+		{
+			_btCom.Disconnect();
+			Assert.IsFalse(_btCom.IsConnected);
+		}
+
+	}
+}
