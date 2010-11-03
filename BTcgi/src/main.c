b4c9cb413f626fffc2c11c2dbf26fcec0a63717e
mattreba <mattreba@786cebc4-e6f9-11dd-9c15-fdd50ba39c1e>
2010-11-03 20:54:21 +0000

    BTcgi Alpha - Updated License Text

--- BTcgi/src/main.c
+++ BTcgi/src/main.c
@@ -1,21 +1,30 @@
 /* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
-/*
- * main.c
- * Copyright (C) Matt Reba 2010 <matt@brewtroller.com>
- * 
- * BTcgi is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * BTcgi is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along
- * with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+/*  
+  BTcgi - BrewTroller JSON CGI Server
+
+  Copyright (C)2010 Open Source Control Systems, Inc.
+
+    This file is part of BrewTroller.
+
+    BrewTroller is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    BrewTroller is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with BrewTroller.  If not, see <http://www.gnu.org/licenses/>.
+
+BrewTroller - Open Source Brewing Computer
+Software Lead: Matt Reba (matt_AT_brewtroller_DOT_com)
+Hardware Lead: Jeremiah Dillingham (jeremiah_AT_brewtroller_DOT_com)
+
+Documentation, Forums and more information available at http://www.brewtroller.com
+*/
 
 #include <stdio.h>
 #include <sys/types.h>
