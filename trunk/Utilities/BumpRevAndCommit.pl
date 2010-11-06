84ba8234a2d81cc7262583e83d1719380956befe
tomontee@gmail.com <tomontee@gmail.com@786cebc4-e6f9-11dd-9c15-fdd50ba39c1e>
2010-11-06 21:08:31 +0000

    Update to BumpRevAndCommit perl script.
      This script should be moved into your root BrewTroller directory. It uses svn status to get a list of
      files that have changed in the given path, or '.' if no path is specified. When the user confims the 
      commit, the script will modify the AssemblyInfo.cs file for .Net projects, or the project.pde file for
      Arduino projects. 
    
      Here are the command line arguments:
    
        Usage: perl BumpRevAndCommit PATH [options]
        
        Valid Options:
          directory path     : path of directory to commit (default = '.'). 
                               :   If no directory is specified, '.' is used.
          --username ARG     : svn User ID
          --password ARG       : svn User password
          --credentials ARG  : path to file w/credentials (2 lines - --username ID, --password PW
                               :    If no credential file is specified, 'svncredentials.txt is used.
          --test               : test mode, updates file revesion but does not do svn commit
          --h (or help)        : print help info
        
          Examples
            BumpRevAndCommit crewTroller
            BumpRevAndCommit -h to display help
    
      If the credential file is not present and the userID and/or password arenot entered on the command line,
      the script will promt for them.
    
      I have been using TortiseSVN. Next step is to try to attach this script into the TotiseSVN pre-commit hook.

--- trunk/Utilities/BumpRevAndCommit.pl
+++ trunk/Utilities/BumpRevAndCommit.pl
@@ -1,40 +1,60 @@
-package BumpRevAndCommit 1;
-# $Revision 1 $
+package BumpRevAndCommit 2;
+# $Revision 2 $
 
 use strict;
 use warnings;
 use File::Spec;
 
-$BumpRevAndCommit::VERSION = 0.1;
+$BumpRevAndCommit::VERSION = 0.2;
 
 {
-    my $userID = 'tomontee@gmail.com';
-    my $password = 'vS7sY9aS5Jd6';
-
-    my $inPath = shift(@ARGV);
-    if (! defined($inPath)) {
-        print "Directory must be specified\n\n";
-        printHelp();
-        exit; 
-    } elsif ($inPath eq "-h") {
-        printHelp();
-        exit;
-    }   
+    my $username;
+    my $password;
+    my $credentials = "svncredentials.txt";
+    my $testMode = 0;
+    my $inPath = ".";
     
-    my $temp = shift(@ARGV);
-    if (defined($temp)) {
-        $userID = $temp;
-    } elsif (!defined($userID)) {
-        print "Enter User ID : ";
-        $userID = <>;
+    # process arguments
+    #
+    while (my $arg = shift @ARGV) {
+        if ($arg =~ m/^--h/i || $arg =~ m/^--help/i) {
+            printHelp();
+            exit; 
+        } elsif ($arg =~ m/^--t/i || $arg =~ m/^--test/i) {
+            $testMode = 1;
+        } elsif ($arg =~ m/^--username/i) {
+            $username = shift @ARGV;
+        } elsif ($arg =~ m/^--password/i) {
+            $password = shift @ARGV;
+        } elsif ($arg =~ m/^--credentials/i) {
+            $credentials = shift @ARGV;
+        } elsif (-e $arg) {
+            $inPath = $arg;
+        } else {
+            if ($arg =~ m/^-+/) {
+                print "Invalid argument: $arg\!\n";
+            } else {
+                print "Invalid commit path: $arg\!\n";
+            }
+            exit;
+        }
     }
-
-    $temp = shift(@ARGV);
-    if (defined($temp)) {
-        $password = $temp;
-    } elsif (!defined($password) ) {
-        print "Enter Password: ";
-        $password = <>;
+    
+    # check for username and password
+    #
+    if (!defined($username)) {
+        $username = getCredentials($credentials, "--username");
+        if (!defined($username)) {
+            print "Enter username : ";
+            $username = <>;
+        }
+    }
+    if (!defined($password)) {
+        $password = getCredentials($credentials, "--password");
+        if (!defined($password)) {
+            print "Enter password : ";
+            $password = <>;
+        }
     }
     
     print "Connecting to SVN...\n";
@@ -60,8 +80,7 @@ $BumpRevAndCommit::VERSION = 0.1;
     # svn dump
     #
     print "\n";
-    print "Commit List (Add, Delete, Modify, Replace\n";
-    print "\nx\n";
+    print "Commit List (Add, Delete, Modify, Replace)\n";
     foreach (@svnCommitList) {
         print "  $_\n";
         $_ =~ s/.\s*//;     # strip off leading status
@@ -70,10 +89,13 @@ $BumpRevAndCommit::VERSION = 0.1;
 
     # confirmation prompt
     #
-    print "Ready to commit revision $revision to SVN\n";
-    print "Press \'C\' to continue or \'X\' to exit.\n";
-    my $char = <>;
-    if ($char !~ m/[cC]/) { exit; }
+    if ($testMode) {
+    } else {
+        print "Ready to commit revision $revision to SVN\n";
+        print "Press \'C\' to continue or \'X\' to exit.\n";
+        my $char = <>;
+        if ($char !~ m/[cC]/) { exit; }
+    }
 
     # change revision of each project file (project.pde or properties\AssemblyInfo.cs)
     #
@@ -90,18 +112,33 @@ $BumpRevAndCommit::VERSION = 0.1;
     }
 
 
-    # execute svn commit command with commit comment editor option
-    system("svn commit --username $userID --password $password --editor-cmd notepad $inPath");
+    # execute svn commit command if not in test mode
+    #
+    if ($testMode) {
+        print "\nTest Mode specified! 'svn commit' step has been skipped.\n"; 
+        print "   Project files have been changed to reflect svn revisiion.\n"; 
+    } else {
+        system("svn commit --username $username --password $password --editor-cmd notepad $inPath");
+        print "'svn commit' complete!";
+    }
+    
 }
 
 # print help
 #
 sub printHelp {
-    print "Bumps Sketch or .net project and checks into BrewTroller SVN\n";
-    print "  Usage\n";
-    print "    arg[0] - directory\n";
-    print "    arg[1] - login\n";
-    print "    arg[2] - password\n";
+    print "Bumps Sketch BUILD or .net project Revision and checks into BrewTroller SVN\n";
+    print "  Usage: perl BumpRevAndCommit PATH [options]\n";
+    print "\n";
+    print "Valid Options:\n";
+    print "  directory path     : path of directory to commit (default = '.').\n";
+    print "                     :   If no directory is specified, '.' is used.\n";
+    print "  --username ARG     : svn User ID\n";
+    print "  --password ARG     : svn User password\n";
+    print "  --credentials ARG  : path to file w/credentials (2 lines - --username ID, --password PW\n";
+    print "                     :    If no credential file is specified, 'svncredentials.txt is used.\n";
+    print "  --test             : test mode, updates file revesion but does not do svn commit\n";
+    print "  --h (or help)      : print help info\n";
     print "\n";
     print "  Examples\n";
     print "    BumpRevAndCommit crewTroller\n";
@@ -109,7 +146,24 @@ sub printHelp {
     return;
 }
 
-
+# get a credential (username or password) from a file
+#
+sub getCredentials {
+    my ($credentialFile, $credentialName) = @_;
+    my $credentialValue;
+    my $file;
+    if (defined($credentialFile) && -e $credentialFile && open($file, q{<}, $credentialFile)) {
+        while (<$file>) {
+            if (m/^\s*$credentialName\s+(.*)\s/i) {
+                $credentialValue = $1;
+                last;
+            }
+         }
+        close $file;
+    }
+    return $credentialValue;
+}
+    
 # get the list of file to be committed using the svn status command
 #
 # ' ' No Modifications
@@ -126,7 +180,9 @@ sub getSvnCommitList {
     my @svnCommitList;
 
     system("svn status $inPath > $statusFileName");
-    open($statusFile, q{<}, $statusFileName);
+    if (!open($statusFile, q{<}, $statusFileName)) {
+        die "Unable to open $statusFileName.\n";
+    }
     while (<$statusFile>) {
         chomp($_);
         if ($_ =~ m/^([admrc])/i) {
@@ -147,7 +203,9 @@ sub getSvnRevision {
     my $infoFile;
     
     system("svn info http://brewtroller.googlecode.com/svn/ > $infoFileName");
-    open($infoFile, q{<}, $infoFileName);
+    if (!open($infoFile, q{<}, $infoFileName)) {
+        die "Unable to open $infoFileName.";
+    }
     while (<$infoFile>) {
         if (m/Revision:\s*(\d*)/) {
             $revision = $1 + 1;
@@ -206,7 +264,6 @@ sub setRevision {
         setRevisionInArduinoProject($rev, $projFile);
     }
 
-    print "Directory: $dir\n";
     $projFile = vsCheck($dir);
     if ($projFile ne q{} ) {
         setRevisionInVisualStudioProject($rev, $projFile);
@@ -238,10 +295,10 @@ sub vsCheck {
     my $safetyValve = 0;
     while ($dir ne q{}) {
         my @projFile = glob $dir."\\*.csproj";
-        if (!defined(@projFile)) {
+        if (!@projFile) {
             @projFile = glob "*\\*.vbproj";
         }
-        if (defined(@projFile)) { last; }
+        if (@projFile) { last; }
         my $index = rindex($dir, q{\\});
         if ($index < 0) {
             $dir = q{};
@@ -260,18 +317,22 @@ sub vsCheck {
     return $projFile;
 }
 
+# set revision number in a Arduion Sketch project
+#
 sub setRevisionInArduinoProject {
     my ($rev, $projFileName) = @_;
     print "Set Revision $rev in Arduino $projFileName\n";
     
     my $projFile;
     if (!open($projFile, q{<}, $projFileName)) {
-        die "Unable to open $projFile\n";
+        die "\nUnable to open $projFileName\n";
     }
 
     my $tempFileName = "buildMaster.pde";
     my $tempFile;
-    open ($tempFile, q{>}, $tempFileName);
+    if (!open ($tempFile, q{>}, $tempFileName)) {
+        die "\nUnable to open $tempFileName\n";
+    }
     
     while (<$projFile>) { 
         if (m/^BUILD/) {
@@ -289,23 +350,37 @@ sub setRevisionInArduinoProject {
     return;
 }
 
+# set revision number in a VisualStudio assembly
+#
 sub setRevisionInVisualStudioProject {
     my ($rev, $projFileName) = @_;
     print "Set Revision $rev in VisualStudio $projFileName\n";
     
     my $projFile;
     if (!open($projFile, q{<}, $projFileName)) {
-        die "Unable to open $projFile\n";
+        die "\nUnable to open $projFileName\n";
     }
 		
     my $tempFileName = $projFileName . ".temp";
     my $tempFile;
-    open ($tempFile, q{>}, $tempFileName);
+    if (!open ($tempFile, q{>}, $tempFileName)) {
+        die "\nUnable to open $tempFileName\n";
+    }
     
     while (<$projFile>) {
         if (m!^[^/+].*(AssemblyVersion|AssemblyFileVersion)!xi) {
+            # pick out the version info Major.Minor.Build.Revision
             m!\"(\d+)\.(\d+)\.(\d+)\.(\d+)\"!;
+            # if revision == $rev, this file has already been changed
+            if ($4 eq $rev) {
+                close $projFile;
+                close $tempFile;
+                unlink $tempFileName;
+                return;
+            }
+            # bump build by 1
             my $build = $3 + 1;
+            # substitue new info
             $_ =~ s!"(\d+)\.(\d+)\.(\d+)\.(\d+)\"!\"$1.$2.$build.$rev\"!ix;
         }
         print $tempFile $_;
