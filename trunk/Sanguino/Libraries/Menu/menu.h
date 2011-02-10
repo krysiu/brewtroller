c432de8c0198f92cd34f3ff6861302b22539eeba
mattreba@gmail.com <mattreba@gmail.com@786cebc4-e6f9-11dd-9c15-fdd50ba39c1e>
2011-02-10 14:41:26 +0000

    Massive updates. Ready for testing.

--- trunk/Sanguino/Libraries/Menu/menu.h
+++ trunk/Sanguino/Libraries/Menu/menu.h
@@ -12,32 +12,57 @@ typedef struct {
 class menu
 {
 public:
-	menu(void);
-	void begin(byte, byte, byte);
-	void clear(void);
-	void addItem(char[], byte);
-	void addItem_P(const char *, byte);
+	/* Constructor: pagesize (rows), maximum menu item count */
+	menu(byte, byte);
+
+	/* Frees menuItems memory */
+	~menu(void);
+
+	/* Adds or updates a menu item (based on unique value) */
+	void setItem(char[], byte);
+	void setItem_P(const char *, byte);
+
+	/* Appends text to an existing menu item */
+	void appendItem(char[], byte);
+	void appendItem_P(const char *, byte);
+
+	/* Set selected by specifying index */
 	void setSelected(byte);
-	byte getSelected(void);
-	boolean refreshDisp(void);
-	void getSelectedRow(char[]);
-	void getVisibleRow(byte, char[]);
-	byte getValue(void);
-	byte getCursor(void);
-	byte getItemCount(void);
+
+	/* Select by menu item value */
 	void setSelectedByValue(byte);
-	void updateItem(char[], byte);
-	void updateItem_P(const char *, byte);
+
+	/* Get selected menu item index */
+	byte getSelected(void);
+
+	/* Update _topItem based on selection and pageSize */
+	boolean refreshDisp(void);
+
+	/* Get specified row's menu item text based on _topItem and _pageSize */
+	void getVisibleRow(byte, char[]);
+
+	/* Get menu item text for currently selected item */
+	char* getSelectedRow(char[]);
+
+	/* Get the value for the currently selected menu item */
+	byte getValue(void);
+
+	/* Get the cursor position based on current selection, _topItem and _pageSize */
+	byte getCursor(void);
+
+	/* Get total number of defined menu items */
+	byte getItemCount(void);
+
+	/* Get menu item index based on specified menu item value */
+	byte getIndexByValue(byte);
 private:
-	byte 	_rows,
-			_cols,
-			_maxOpts,
-			_itemCount,
-			_selected,
-			_topItem;
+	byte 	_pageSize,
+		_maxOpts,
+		_itemCount,
+		_selected,
+		_topItem;
 			
 	menuItem *_menuItems;
-	byte getIndexByValue(byte);
 };
 
 #endif
