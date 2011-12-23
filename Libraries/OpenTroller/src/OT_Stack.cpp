fb2edd8ddc99733db3e5c9f20d87106818cdd696
treaves <treaves@silverfieldstech.com>
2011-12-23 21:18:22 +0000

    Corrected an import: should have been OTOutputs.h, not OTOutput.h .

--- Libraries/OpenTroller/src/OT_Stack.cpp
+++ Libraries/OpenTroller/src/OT_Stack.cpp
@@ -1,15 +1,52 @@
+/*
+    Copyright (C) 2011 Matt Reba (mattreba at oscsys dot com)
+    Copyright (C) 2011 Timothy Reaves (treaves at silverfieldstech dot com)
+
+    This file is part of OpenTroller.
+
+    OpenTroller is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenTroller is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenTroller.  If not, see <http://www.gnu.org/licenses/>.
+
+
+*/
 #include "OT_Stack.h"
 
-void* operator new(size_t size) { return malloc(size); }
-void operator delete(void* ptr) { free(ptr); }
-void * operator new[](size_t size) { return malloc(size); }
-void operator delete[](void * ptr) { if (ptr) free(ptr); }
+#include "OT_HWProfile.h"
+#include "outputs/OTOutputs.h"
+
+void* operator new(size_t size) {
+    return malloc(size);
+}
+
+void operator delete(void* ptr) {
+    free(ptr);
+}
+
+void * operator new[](size_t size) {
+    return malloc(size);
+}
+
+void operator delete[](void * ptr) {
+    if (ptr) {
+        free(ptr);
+    }
+}
 
 using namespace OpenTroller;
 
 void stack::init() {
 	#ifdef OPENTROLLER_OUTPUTS
-		OpenTroller::Outputs.init();
+        OpenTroller::Outputs.init();
 	#endif
 	
 	#if defined OPENTROLLER_LCD4BIT || defined OPENTROLLER_LCDI2C
--- Libraries/OpenTroller/src/OT_Stack.h
+++ Libraries/OpenTroller/src/OT_Stack.h
@@ -1,19 +1,41 @@
-#ifndef OT_STACK_H
-	#define OT_STACK_H
-	#include "OT_Encoder.h"
-	#include "OT_HWProfile.h"
-    #include "outputs/OTOutput.h"
-	#include "OT_LCD.h"
-	#include "OT_Status.h"
+/*
+    Copyright (C) 2011 Matt Reba (mattreba at oscsys dot com)
+    Copyright (C) 2011 Timothy Reaves (treaves at silverfieldstech dot com)
 
-	namespace OpenTroller {
-		class stack
-		{
-			public:
-			void init();
-			void update();
-			
-		};
-		extern OpenTroller::stack Stack;
-	}
-#endif
+    This file is part of OpenTroller.
+
+    OpenTroller is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenTroller is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenTroller.  If not, see <http://www.gnu.org/licenses/>.
+
+
+*/
+#ifndef OT_STACK_H
+#define OT_STACK_H
+
+#include "OT_Encoder.h"
+#include "OT_HWProfile.h"
+#include "OT_LCD.h"
+#include "OT_Status.h"
+
+namespace OpenTroller {
+
+class stack {
+  public:
+    void init();
+    void update();
+
+};
+
+extern OpenTroller::stack Stack;
+}
+#endif // OT_STACK_H
