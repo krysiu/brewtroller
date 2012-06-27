2c20e70c3ea559313abb75438329eb8423aa83d2
mattreba@gmail.com <mattreba@gmail.com@786cebc4-e6f9-11dd-9c15-fdd50ba39c1e>
2012-06-27 17:52:42 +0000

    Updated output/alarm pins for EX1 profiles.

--- trunk/BrewTroller/BrewTroller.pde
+++ trunk/BrewTroller/BrewTroller.pde
@@ -1,4 +1,4 @@
-#define BUILD 953
+#define BUILD 954
 /*  
   Copyright (C) 2009, 2010 Matt Reba, Jeremiah Dillingham
 
--- trunk/BrewTroller/HWProfiles/OpenTroller EX1/RIMS or Direct Fired/HWProfile.h
+++ trunk/BrewTroller/HWProfiles/OpenTroller EX1/RIMS or Direct Fired/HWProfile.h
@@ -12,13 +12,14 @@ OpenTroller EX1 Hardware Configuration
   #define ENTER_PIN 1
   #define ENCODER_ACTIVELOW
   
-  #define ALARM_PIN 15 //OUT6
+  #define ALARM_PIN 27 //EX1 Alarm
   
   #define PVOUT_TYPE_GPIO
-  #define PVOUT_COUNT 2 //2 Outputs
+  #define PVOUT_COUNT 3 //2 Outputs
   
   #define VALVE1_PIN 19 //OUT4
   #define VALVE2_PIN 18 //OUT5
+  #define VALVE3_PIN 15 //OUT6
 
   #define HLTHEAT_PIN 22 //OUT1
   #define MASHHEAT_PIN 21//OUT2
