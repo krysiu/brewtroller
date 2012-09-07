de878828499dc047d0a805b06132b798b04d848b
mattreba <mattreba@gmail.com>
2012-09-07 15:47:27 +0000

    Updated test scripts for MODBUS Relay 8

--- MODBUS_Relay8/test_scripts/Slave Address Clear.cmd
+++ MODBUS_Relay8/test_scripts/Slave Address Clear.cmd
@@ -1,5 +1,5 @@
 @echo off
 call settings.cmd
-modpoll -m rtu -b %mbp_baud% -a %mbp_slaveaddr% -r %mbp_regslaveaddr% -t 4 -1 %mbp_com% %mbp_defslaveaddr%
-modpoll -m rtu -b %mbp_baud% -a %mbp_slaveaddr% -r %mbp_regrestart% -t 4 -1 %mbp_com% 1
+modpoll -m rtu -b %mbp_baud% -p %mbp_parity% -a %mbp_slaveaddr% -r %mbp_regslaveaddr% -t 4 -1 %mbp_com% %mbp_defslaveaddr%
+modpoll -m rtu -b %mbp_baud% -p %mbp_parity% -a %mbp_slaveaddr% -r %mbp_regrestart% -t 4 -1 %mbp_com% 1
 pause
--- MODBUS_Relay8/test_scripts/Slave Address Set.cmd
+++ MODBUS_Relay8/test_scripts/Slave Address Set.cmd
@@ -1,5 +1,5 @@
 @echo off
 call settings.cmd
-modpoll -m rtu -b %mbp_baud% -a %mbp_defslaveaddr% -r %mbp_regslaveaddr% -t 4 -1 %mbp_com% %mbp_slaveaddr%
-modpoll -m rtu -b %mbp_baud% -a %mbp_defslaveaddr% -r %mbp_regrestart% -t 4 -1 %mbp_com% 1
+modpoll -m rtu -b %mbp_baud% -p %mbp_parity% -a %mbp_defslaveaddr% -r %mbp_regslaveaddr% -t 4 -1 %mbp_com% %mbp_slaveaddr%
+modpoll -m rtu -b %mbp_baud% -p %mbp_parity% -a %mbp_defslaveaddr% -r %mbp_regrestart% -t 4 -1 %mbp_com% 1
 pause
