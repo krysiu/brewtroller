69c388e8d1c093d46b8a84074f963f3923cbd069
mattreba <mattreba@gmail.com>
2012-11-26 01:19:20 +0000

    First commit of OpenTroller SDK (work in progress). Much of this code will serve as the base for BrewTroller 3.0

---
+++ OpenTrollerSDK/OT_Inputs.h
@@ -0,0 +1,279 @@
+/*  
+   Copyright (C) 2009, 2010 Matt Reba, Jeremiah Dillingham
+
+    This file is part of BrewTroller.
+
+    BrewTroller is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    BrewTroller is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with BrewTroller.  If not, see <http://www.gnu.org/licenses/>.
+
+
+BrewTroller - Open Source Brewing Computer
+Software Lead: Matt Reba (matt_AT_brewtroller_DOT_com)
+Hardware Lead: Jeremiah Dillingham (jeremiah_AT_brewtroller_DOT_com)
+
+Documentation, Forums and more information available at http://www.brewtroller.com
+*/
+#ifndef OT_Inputs_h
+#define OT_Inputs_h
+
+#include <stdint.h>
+#include <string.h>
+#include <math.h>
+#include "Config.h"
+#include "HWProfile.h"
+#include <WProgram.h>
+#include <pin.h>
+#include <ModbusMaster.h>
+#ifdef TS_ONEWIRE
+  #if defined TS_ONEWIRE_GPIO
+    #include <OneWire.h>
+  #elif defined TS_ONEWIRE_I2C
+    #include <DS2482.h>
+  #endif
+#endif
+
+#define ANALOGIN_NAME_MAXLEN 20
+
+typedef struct calibration_t {
+  unsigned int value;
+  long actual;
+};
+
+typedef enum {
+  analogInType_None,
+  analogInType_GPIO,
+  analogInType_Modbus
+} analogInType;
+
+typedef struct analogInCfg_t {
+  analogInType type;
+  union {
+    struct analogInCfg_GPIO_t {
+      uint8_t pin;
+    } analogInCfg_GPIO;
+    struct analogInCfg_Modbus_t {
+      uint8_t slaveAddr;
+      unsigned int reg;
+    } analogInCfg_Modbus;
+  } implementation;
+  bool calibrate;
+  calibration_t calibrations[ANALOG_CALIBRATION_COUNT];
+  uint8_t averaging;
+};
+
+class calibratedValue {
+  private:
+  calibration_t * calibrationTable;
+  uint8_t count;
+  
+  public:
+  calibratedValue(uint8_t c, calibration_t entries[]);
+  ~calibratedValue();
+  long compute(unsigned int value);
+};
+
+class averagedValue {
+  private:
+  uint8_t count, index;
+  long * values;
+  
+  public:
+  averagedValue(uint8_t c);
+  ~averagedValue();
+  
+  long compute(long value);
+};
+
+class analogIn {
+  protected:
+  long value;
+  calibratedValue* cValue;
+  averagedValue* aValue;
+  
+  public:
+  analogIn(void);
+  ~analogIn();
+  static analogIn* create(analogInCfg_t cfg);
+  virtual long getValue();
+  virtual void setupCalibration(calibratedValue * c);
+  virtual void setupAverages(averagedValue * a);
+  virtual void init(void);
+  virtual void update(void) {}
+};
+
+#ifdef ANALOGINPUTS_GPIO
+class analogIn_GPIO : public analogIn {
+  private:
+  uint8_t pin;
+  
+  public:
+  analogIn_GPIO(uint8_t pinNum);
+  void update();
+  //Utility Functions
+  static uint8_t getCount();
+  static uint8_t getPin(uint8_t index);
+  static char* getName(uint8_t index, char* retString);  
+};
+#endif
+
+typedef enum {
+  tSensorType_None,
+  tSensorType_1Wire,
+  tSensorType_Modbus
+} tSensorType;
+  
+typedef struct tSensorCfg_t {
+  tSensorType type;
+  union {
+    struct tSensorCfg_1Wire_t {
+      byte addr[8];
+    } tSensorCfg_1Wire;
+    struct tSensorCfg_Modbus_t {
+      byte slaveAddr;
+      unsigned int reg;
+    } tSensorCfg_Modbus;
+  } implementation;
+};
+
+typedef struct oneWireBusCfg_t {
+  boolean parasite;
+  boolean crcCheck;
+  boolean resLow;
+  boolean resHigh;
+};
+
+#define BAD_TEMP -2147483648
+
+class tSensor : public analogIn {
+  protected:
+  static bool unitMetric;
+    
+  public:
+  tSensor(void);
+  ~tSensor(void) {}
+  static tSensor* create(tSensorCfg_t cfg);
+  long getValue();
+  static void setUnit(bool unit);
+  static bool getUnit() { return unitMetric; }
+  virtual void init(void) { };
+  virtual void update(void) {}
+};
+
+#ifdef TS_ONEWIRE
+
+  #define DS18B20_DELAY { 94, 188, 375, 750 } //9Bit - 12Bit
+
+  class tSensor_1Wire : public tSensor {
+    private:
+    static unsigned long convStart;
+    static unsigned int convDelay;
+    static byte resolution;
+    static boolean parasitePwr, crcCheck;
+    static tSensor_1Wire* sensorHead;
+    #if defined TS_ONEWIRE_GPIO
+      static OneWire *ds;
+    #elif defined TS_ONEWIRE_I2C
+      static DS2482 *ds;
+    #endif
+    byte addr[8];
+    tSensor_1Wire* sensorNext;
+
+    void attach(tSensor_1Wire* tsMe);
+    void detach(tSensor_1Wire* tsMe, tSensor_1Wire* tsNext);
+    boolean validAddr();
+    void readTemp();
+    static boolean isReady();
+    bool matchAddr(byte* scanAddr);
+   
+    public:
+    tSensor_1Wire(byte *tsAddr);
+    ~tSensor_1Wire();
+    void init();  
+    void update();
+
+
+    
+    //Utility (static) functions
+    #if defined TS_ONEWIRE_GPIO
+      static void setup(OneWire *bus, bool parasite, bool crc, byte res);
+    #elif defined TS_ONEWIRE_I2C
+      static void setup(DS2482 *bus, bool parasite, bool crc, byte res);
+    #endif
+
+    static void scanBus(byte* addrRet, byte limit = 0, bool skipAssigned = true);
+  };
+#endif
+
+typedef enum {
+  TRANSITION_CHANGE = CHANGE,
+  TRANSITION_FALLING = FALLING,
+  TRANSITION_RISING = RISING
+} transitionType;
+
+typedef enum {
+  triggerType_None,
+  triggerType_GPIO,
+  triggerType_Modbus
+} triggerType;
+
+typedef struct triggerCfg_t {
+  triggerType type;
+  union {
+    struct triggerCfg_GPIO_t {
+      byte pin;
+      transitionType mode;
+    } triggerCfg_GPIO;
+    struct triggerCfg_Modbus_t {
+      byte slaveAddr;
+      unsigned int dataAddr;
+      transitionType mode;
+    } triggerCfg_Modbus;
+  } implementation;
+};
+
+
+class trigger {
+  protected:
+  transitionType mode;
+  boolean value, lastValue;
+  
+  public:
+  static trigger* create(triggerCfg_t cfg);
+  virtual boolean getState();
+  virtual void init();
+  virtual void update();
+};
+
+
+
+class trigger_GPIO : public trigger {
+  private:
+  pin trigPin;
+  
+  public:
+  trigger_GPIO(byte pinNum, transitionType m);
+  void update();
+};
+
+
+class trigger_Modbus : public trigger {
+  private:
+  ModbusMaster slave;
+  unsigned int dataAddr;
+    
+  public:
+  trigger_Modbus(byte sAddr, unsigned int dAddr, transitionType m);
+  void update();
+};
+
+#endif
---
+++ OpenTrollerSDK/OpenTrollerSDK.pde
@@ -0,0 +1,117 @@
+#include <ScreenUi.h>
+#include <LiquidCrystalFP.h>
+#include <pin.h>
+#include <encoder.h>
+#include <Wire.h>
+#include <ModbusMaster.h>
+#include "HWProfile.h"
+#include <NewDelete.h>
+#include <DS2482.h>
+#include <OneWire.h>
+#include "OT_UI.h"
+#include "OT_Inputs.h"
+#include "OT_Outputs.h"
+
+//Define 1-Wire Bus Object
+#ifdef TS_ONEWIRE
+  #if defined TS_ONEWIRE_GPIO
+    #include <OneWire.h>
+    OneWire ds(TEMP_PIN);
+  #elif defined TS_ONEWIRE_I2C
+    #include <DS2482.h>
+    DS2482 ds(DS2482_ADDR);
+  #endif
+#endif
+
+outputs Outputs;
+
+//Declare our worker function
+void testCore(void);
+
+//Create UI and pass worker function
+screenUI ScreenUI(testCore);
+
+void setup() {
+  #ifdef I2C_SUPPORT
+    Wire.begin(BT_I2C_ADDR);
+    #ifdef TS_ONEWIRE_I2C
+      ds.configure(DS2482_CONFIG_APU | DS2482_CONFIG_SPU);
+    #endif
+  #endif
+
+  //Attach Outputs to SWPWM
+  analogOutput_SWPWM::setup(&Outputs);
+  
+  //Attach 1-Wire bus to tSensor_1Wire
+  tSensor_1Wire::setup(&ds, 1, 1, 1);
+  
+  //Add Modbus bank
+  Outputs.newModbusBank(10,1000,8);
+  
+  delay(1000);
+  Wire.begin();
+  pinMode(LCD_BRIGHT_PIN, OUTPUT);
+  pinMode(LCD_CONTRAST_PIN, OUTPUT);
+  TCCR2B = 0x01;
+  analogWrite(LCD_BRIGHT_PIN, 255 - LCD_DEFAULT_BRIGHTNESS);
+  analogWrite(LCD_CONTRAST_PIN, LCD_DEFAULT_CONTRAST);
+  Serial.begin(115200);
+}
+
+unsigned long outProfileX = 0;
+byte pwmPinX = 0;
+tSensorCfg_t sensorXCfg;
+analogInCfg_t aInXCfg;
+
+void mainMenu() {
+  Screen * screen = ScreenUI.create(20, 4);
+  Label titleLabel("ScreenUI Main Menu");
+  screen->add(&titleLabel, 0, 0);
+  ScrollContainer scrollContainer(screen, screen->width(), 3);
+  
+  Button pwmPinBtn("Select Output");
+  scrollContainer.add(&pwmPinBtn, 0, 1);
+  
+  Button profileXBtn("Cfg Output Profile");
+  scrollContainer.add(&profileXBtn, 0, 2);
+
+  Button tSensorBtn("Cfg Temp Sensor");
+  scrollContainer.add(&tSensorBtn, 0, 3);
+
+  Button aInBtn("Cfg AnalogIn");
+  scrollContainer.add(&aInBtn, 0, 4);
+
+  
+  screen->add(&scrollContainer, 0, 1);
+  
+  while (1) {
+    screen->update();
+    if (pwmPinBtn.pressed()) {
+      pwmPinX = ScreenUI.dlgSelectOutput("Select Output", &Outputs, pwmPinX);
+      screen->clear(); screen->repaint();
+    }
+    else if (profileXBtn.pressed()) {
+      outProfileX = ScreenUI.dlgCfgOutputProfile("Cfg Output Profile", &Outputs, outProfileX);
+      screen->clear(); screen->repaint();
+    }
+    else if (tSensorBtn.pressed()) {
+      ScreenUI.dlgCfgTSensor("Cfg Temp Sensor", &sensorXCfg);
+      screen->clear(); screen->repaint();
+    }
+    else if (aInBtn.pressed()) {
+      ScreenUI.dlgCfgAnalogIn("Cfg Analog Input", &aInXCfg);
+      screen->clear(); screen->repaint();
+    }
+    //Call worker process
+    ScreenUI.wait();
+  }  
+}
+
+void loop() {
+  mainMenu();
+}
+
+void testCore (void) {
+  Outputs.update();
+}
+
