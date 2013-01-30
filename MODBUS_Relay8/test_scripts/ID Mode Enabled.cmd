e846a9a32441006681d90b502ef4e568aa17d2d7
mattreba <mattreba@gmail.com>
2013-01-30 18:50:16 +0000

    Removing Modbus Relay Trunk

--- MODBUS_Relay8/test_scripts/ID Mode Enabled.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-call settings.cmd
-modpoll -m rtu -b %mbp_baud% -p %mbp_parity% -a %mbp_slaveaddr% -r %mbp_regidmode% -t 4 -1 %mbp_com% 1
-pause
--- MODBUS_Relay8/test_scripts/Slave Address Clear.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-call settings.cmd
-modpoll -m rtu -b %mbp_baud% -p %mbp_parity% -a %mbp_slaveaddr% -r %mbp_regslaveaddr% -t 4 -1 %mbp_com% %mbp_defslaveaddr%
-modpoll -m rtu -b %mbp_baud% -p %mbp_parity% -a %mbp_slaveaddr% -r %mbp_regrestart% -t 4 -1 %mbp_com% 1
-pause
